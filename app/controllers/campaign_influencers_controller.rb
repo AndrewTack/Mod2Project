cce183161a039c46906a353c2e7b1bc1fd3817c1
github username <github email address>
2019-08-20 13:51:39 -0400

    campaign_influencer controller work

--- app/controllers/campaign_influencers_controller.rb
+++ app/controllers/campaign_influencers_controller.rb
@@ -1,2 +1,52 @@
 class CampaignInfluencersController < ApplicationController
+
+    def index
+        @campaign_influencers = CampaignInfluencer.all
+    end
+
+    def show
+        @campaign_influencer = CampaignInfluencer.find(params[:id])
+    end
+
+    def new
+        @campaign_influencer = CampaignInfluencer.new
+    end
+
+    def create
+        @campaign_influencer = CampaignInfluencer.create(campaign_influencer_params)
+        if @campaign_influencer.valid?
+            redirect_to @campaign_influencer
+        else 
+            flash[:errors] = @campaign_influencer.errors.full_messages
+            redirect_to new_campaign_influencer_path
+        end 
+    end
+
+    def edit
+        @campaign_influencer = CampaignInfluencer.find(params[:id])
+    end
+
+    def update
+        @campaign_influencer = CampaignInfluencer.find(params[:id])
+        if @campaign_influencer.update(campaign_influencer_params)
+            redirect_to @campaign_influencer
+        else 
+            flash[:errors] = @campaign_influencer.errors.full_messages
+            redirect_to edit_campaign_influencer_path(@campaign_influencer)
+        end     
+    end
+
+    def destroy
+        @campaign_influencer = CampaignInfluencer.find(params[:id])
+        @campaign_influencer.destroy
+
+        redirect_to campaign_influencers_path
+    end
+
+    private
+
+    def campaign_influencer_params
+        params.require(:campaign_influencer).permit(:campaign_id, :influencer_id, :approval_status, :pitch)
+    end
+
 end
--- app/models/campaign_influencer.rb
+++ app/models/campaign_influencer.rb
@@ -3,4 +3,6 @@ class CampaignInfluencer < ApplicationRecord
     belongs_to :campaign
     belongs_to :influencer
 
+    validates :pitch, length: {minimum: 50}
+
 end
